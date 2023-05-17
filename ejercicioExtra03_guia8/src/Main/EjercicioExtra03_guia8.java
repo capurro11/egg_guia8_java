b60ef842793320257a52794007bd4434fa39e611
capurro11 <acapurroluz@gmail.com>
2023-05-16 23:33:54 -0300

    guardando cambios

--- ejercicioExtra03_guia8/src/Main/EjercicioExtra03_guia8.java
+++ ejercicioExtra03_guia8/src/Main/EjercicioExtra03_guia8.java
@@ -44,7 +44,6 @@ public class EjercicioExtra03_guia8 {
         
         // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
         
-        sr.Mostrar();
     }
 
 }
